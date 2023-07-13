00bb4f101cc841fb237f2fccc30b34e068a9a2be
Andres Alonso <andresalonso.a@gmail.com>
2023-07-13 12:56:36 -0300

    agregando valores necesarios para edicion de turnos en front

--- controllers/sala_grupo.go
+++ controllers/sala_grupo.go
@@ -12,8 +12,10 @@ import (
 type SalaGrupoAgenda struct {
 	ID        int    `json:"id"`
 	Sala      string `json:"sala" binding:"required"`
+	SalaId    int    `json:"sala_id" binding:"required"`
 	SalaColor int    `json:"sala_color" binding:"required"`
 	Grupo     string `json:"grupo" binding:"required"`
+	GrupoId   int    `json:"grupo_id" binding:"required"`
 	HoraDesde string `json:"hora_desde" binding:"required"`
 	HoraHasta string `json:"hora_hasta" binding:"required"`
 }
@@ -30,7 +32,7 @@ func GetSalaGrupo(c *gin.Context) {
 	fecha := c.Query("fecha")
 
 	db := sqldb.ConnectDB()
-	res, err := db.Query(`SELECT sg.id, s.nombre sala, s.color sala_color, g.nombre grupo, sg.hora_desde, sg.hora_hasta 
+	res, err := db.Query(`SELECT sg.id, s.nombre sala, s.id sala_id, s.color sala_color, g.nombre grupo, g.id grupo_id, sg.hora_desde, sg.hora_hasta 
 		FROM sala_grupo sg 
 		JOIN sala s ON sg.sala_id = s.id 
 		JOIN grupo g ON sg.grupo_id = g.id
@@ -46,7 +48,7 @@ func GetSalaGrupo(c *gin.Context) {
 	for res.Next() {
 		var salaGrupo SalaGrupoAgenda
 		var err error
-		err = res.Scan(&salaGrupo.ID, &salaGrupo.Sala, &salaGrupo.SalaColor, &salaGrupo.Grupo, &salaGrupo.HoraDesde, &salaGrupo.HoraHasta)
+		err = res.Scan(&salaGrupo.ID, &salaGrupo.Sala, &salaGrupo.SalaId, &salaGrupo.SalaColor, &salaGrupo.Grupo, &salaGrupo.GrupoId, &salaGrupo.HoraDesde, &salaGrupo.HoraHasta)
 		if err != nil {
 			helpers.Error.Println(err.Error())
 			c.JSON(http.StatusUnprocessableEntity, gin.H{"error": "Error al validar datos: " + err.Error()})
